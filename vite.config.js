0db928004768c3c74cf26081b1bef2a3d82451a2
Dmytro <dimmmashevchenko@gmail.com>
2025-08-28 19:26:48 +0300

    Header build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@
 import { defineConfig } from "vite";
 import path from "path";
 
+
 export default defineConfig({
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),          // для src
