356778aedc737b8eee7b97be1bab99b36a73749a
Dmytro <dimmmashevchenko@gmail.com>
2025-08-28 20:02:00 +0300

    Add vite config plugin-react

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@
 
 import { defineConfig } from "vite";
 import path from "path";
-
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-
+ base: '/mini-store/',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),          // для src
