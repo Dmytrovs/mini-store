5b8519c9d330658aab375f3a14320d311b4b62a7
Dmytro <dimmmashevchenko@gmail.com>
2025-08-28 19:05:04 +0300

    Header burger menu

---
+++ src/components/Header/header.js
@@ -0,0 +1,68 @@
+import "./header.scss";
+
+// Подтягуємо HTML як строку 
+import headerHtml from "./header.html?raw";
+
+
+
+function bodyLock() {
+  document.body.style.overflow = 'hidden';
+}
+
+function bodyUnlock() {
+  document.body.style.overflow = 'auto';
+}
+
+function bodyLockToggle() {
+  if (document.body.style.overflow === 'hidden') {
+    bodyUnlock()
+  } else {
+    bodyLock()
+  }
+}
+
+
+
+export function renderHeader() {
+  document.querySelector("#header").innerHTML += headerHtml;
+  const btns = document.querySelectorAll('.menu__toggle-btn')
+
+  btns.forEach(button => {
+    button.addEventListener('click', (event) => {
+      event.stopPropagation(); // Зупинити подальшу обробку події
+      const subMenu = button.nextElementSibling;
+      const expanded = button.getAttribute('aria-expanded') === 'true';
+
+      button.classList.toggle('open');
+
+
+      //Відкрити поточне підменю
+      button.setAttribute('aria-expanded', !expanded);
+      subMenu.classList.toggle('open', !expanded);
+    });
+  });
+
+  // Закрити всі підменю при кліку поза ними
+  document.addEventListener('click', () => {
+    btns.forEach(button => { 
+      button.setAttribute('aria-expanded', 'false');
+      button.nextElementSibling.classList.remove('open');
+      button.classList.remove('open');
+    });
+  }); 
+
+  // Логіка менб бургер
+    if (document.querySelector(".icon-menu")) {
+      document.addEventListener("click", function (e) {
+        if (e.target.closest('.icon-menu')) {
+          bodyLockToggle();
+          document.documentElement.classList.toggle("menu-open");
+        }
+      });
+    };
+}
+
+
+renderHeader();
+
+
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+
+
+
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),          // для src
+      "@components": path.resolve(__dirname, "src/components"),
+      "@scss": path.resolve(__dirname, "src/scss"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //  "глобальні" стилі
+        includePaths: [path.resolve(__dirname, "src/scss")],
+        additionalData: `
+          @use "@scss/colors" as *;
+          @use "@scss/mixins" as *;
+          @use "@scss/fonts" as *;
+        `
+      },
+    },
+  },
+});
+
